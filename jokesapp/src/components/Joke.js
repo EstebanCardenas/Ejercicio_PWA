f07bf58c415917ca08683bcb45b72a7fb25eb0c6
EstebanCardenas <53832358+EstebanCardenas@users.noreply.github.com>
2020-11-26 02:35:27 -0500

    first commit

---
+++ jokesapp/src/components/Joke.js
@@ -0,0 +1,75 @@
+import React, {useState, useEffect} from 'react'
+import md5 from 'md5'
+import {Card, CardDeck, Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const publicK = process.env.REACT_APP_PUBLIC_KEY
+const privateK = process.env.REACT_APP_PRIVATE_KEY
+const ts = String(Date.now())
+const params = {
+    "apikey": publicK,
+    "ts": ts,
+    "hash": md5(ts+privateK+publicK)
+}
+
+function Hero(props) {
+    const img_url = "https://res.cloudinary.com/heroespatches/image/upload/v1546970566/hero/imperius.png"
+    //const img_url = new URL(props["img"])
+    //img_url.search = new URLSearchParams(params).toString()
+    return (
+        <Col>
+            <Card style={{width:'18rem', marginTop: "10px"}}>
+                <Card.Img variant="top" src={img_url}/>
+                <Card.Body>
+                    <Card.Title>{props["title"]}</Card.Title>
+                    <Card.Text>
+                        {props["desc"]}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
+    )
+}
+
+function Heroes() {
+    const [header, setHeader] = useState("")
+    const [heroes, setHeroes] = useState([])
+
+    useEffect(()=>{
+        if(!navigator.onLine){
+            if(localStorage.getItem("heroes") === null) {
+                setHeader("Loading...")
+            } else {
+                setHeader("Heroes")
+                setHeroes(localStorage.getItem("heroes"));
+            }
+        } else {
+            
+            const url =  new URL("https://gateway.marvel.com:443/v1/public/characters")
+            url.search = new URLSearchParams(params).toString()
+            fetch(url).then(res=>res.json()).then(res=>{
+                setHeader("Heroes")
+                setHeroes(Array.from(res["data"]["results"]))
+                localStorage.setItem("heroes", res.value)
+            })
+        }
+    }, [])
+
+    return (<Container fluid>
+        <h1 style={{textAlign: "center"}}>{header}</h1>
+        <Row>
+            <CardDeck>
+                {heroes.map((e, i) => {
+                    return (<Hero
+                        key={i}
+                        img={e["thumbnail"]["path"]}
+                        title={e["name"]}
+                        desc={e["description"]}
+                    />)
+                })}
+            </CardDeck>
+        </Row>
+    </Container>)
+}
+
+export default Heroes
